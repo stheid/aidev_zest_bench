66b28d46b111527f73d611ca059372220aa43225
Stefan Heid <stefan.heid@upb.de>
2022-06-27 10:56:44 +0200

    add shadowJar build add maven + generator fuzztarget

---
+++ src/main/kotlin/fuzztargets/maven_gen.kt
@@ -0,0 +1,34 @@
+package fuzztargets
+
+import edu.berkeley.cs.jqf.examples.xml.XMLDocumentUtils
+import edu.berkeley.cs.jqf.examples.xml.XmlDocumentGenerator
+import edu.berkeley.cs.jqf.fuzz.guidance.StreamBackedRandom
+import edu.berkeley.cs.jqf.fuzz.junit.quickcheck.FastSourceOfRandomness
+import edu.berkeley.cs.jqf.fuzz.junit.quickcheck.NonTrackingGenerationStatus
+import org.apache.maven.model.Model
+import org.apache.maven.model.io.DefaultModelReader
+import org.apache.maven.model.io.ModelReader
+import org.w3c.dom.Document
+import java.io.ByteArrayInputStream
+import java.io.IOException
+
+
+@Suppress("unused")
+class ParsePomFuzzer {
+    @Throws(Exception::class)
+    fun fuzzerTestOneInput(input: ByteArray?) {
+        val `is` = ByteArrayInputStream(input)
+        // Generate input values
+        val randomFile = StreamBackedRandom(`is`, java.lang.Long.BYTES)
+        val random = FastSourceOfRandomness(randomFile)
+        val genStatus = NonTrackingGenerationStatus(random)
+        val gen = XmlDocumentGenerator()
+        val reader: ModelReader = DefaultModelReader()
+        val dom: Document = gen.generate(random, genStatus)
+        try {
+            @Suppress("UNUSED_VARIABLE")
+            val model: Model = reader.read(XMLDocumentUtils.documentToInputStream(dom), null)
+        } catch (ignore: IOException) {
+        }
+    }
+}
