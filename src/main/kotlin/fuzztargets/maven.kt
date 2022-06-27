1ede42223f09cf6edb7bcc8dcea6356bc9036841
Stefan Heid <stefan.heid@upb.de>
2022-06-27 09:36:43 +0200

    add gradle build harness add Maven fuzztarget

---
+++ src/main/kotlin/fuzztargets/maven.kt
@@ -0,0 +1,19 @@
+package fuzztargets
+
+import org.apache.maven.model.io.DefaultModelReader
+import java.io.ByteArrayInputStream
+import java.io.IOException
+
+@Suppress("unused")
+class MavenFuzzer {
+    @Throws(Exception::class)
+    fun fuzzerTestOneInput(input: ByteArray?) {
+        val stream = ByteArrayInputStream(input)
+        val reader = DefaultModelReader()
+        try {
+            @Suppress("UNUSED_VARIABLE")
+            val model = reader.read(stream, null)
+        } catch (ignore: IOException) {
+        }
+    }
+}
