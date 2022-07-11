2e135a665e18635bc78f6085d382916da3f0bd39
Stefan Heid <stefan.heid@upb.de>
2022-07-11 17:13:23 +0200

    reordered files

--- src/main/kotlin/fuzztargets/jqf/closure_gen.kt
+++ src/main/kotlin/fuzztargets/jqf/closure_gen.kt
@@ -3,7 +3,7 @@ import edu.berkeley.cs.jqf.examples.js.JavaScriptCodeGenerator
 
 
 @Suppress("unused")
-object ClosureGeneratorFuzzer {
+object ClosureGenFuzzer {
     @JvmStatic
     fun fuzzerTestOneInput(input: ByteArray) =
         ClosureFuzzer.compile(generate(input, JavaScriptCodeGenerator()))
