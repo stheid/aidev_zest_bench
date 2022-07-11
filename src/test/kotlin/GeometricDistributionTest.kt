0f693ed1084ba415b9c99ca350a934aadf247372
Stefan Heid <stefan.heid@upb.de>
2022-07-11 17:15:04 +0200

    create a simple test to show how we can create the bytes to result in the structural values we want

--- src/test/kotlin/GeometricDistributionTest.kt
+++ src/test/kotlin/GeometricDistributionTest.kt
@@ -21,6 +21,7 @@ class GeometricDistributionTest {
         val dist = GeometricDistribution()
         assert(1 == dist.sampleWithMean(10.0, random))
     }
+
     @Test
     fun testSampleWithMeanTwo() {
         val stream = ByteArrayInputStream(seed + byteArrayOf(31, -75, -73, -128, -102, 100, -80, 63))
@@ -31,12 +32,21 @@ class GeometricDistributionTest {
     }
 
     @Test
+    fun testSampleWithMeanThree() {
+        val stream = ByteArrayInputStream(seed + byteArrayOf(100, 59, -22, -80, -72, -3, 0, -100))
+        val randomFile = StreamBackedRandom(stream)
+        val random = FastSourceOfRandomness(randomFile)
+        val dist = GeometricDistribution()
+        assert(3 == dist.sampleWithMean(10.0, random))
+    }
+
+    @Test
     fun testFindSampleWithMean() {
+        val goal = 10
         var result = 0
         val bytes = ByteArray(8)
-        while (result != 2) {
-            val long = Random().nextLong()
-            ByteBuffer.wrap(bytes).putLong(long)
+        while (result != goal) {
+            ByteBuffer.wrap(bytes).putLong(Random().nextLong())
             val stream = ByteArrayInputStream(seed + bytes)
             val randomFile = StreamBackedRandom(stream)
             val random = FastSourceOfRandomness(randomFile)
@@ -44,6 +54,6 @@ class GeometricDistributionTest {
             result = dist.sampleWithMean(10.0, random)
         }
         println(bytes.toList().joinToString(prefix = "byteArrayOf(", postfix = ")"))
-        assert(2 == result)
+        assert(goal == result)
     }
 }
